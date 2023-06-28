10d5ad3c75813d97cf951291be75601d3e41e0ce
anh204809 <anh.nt211202@gmail.com>
2023-06-29 00:45:24 +0700

    Server: create UsersModule and AuthModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'project_tiktok',
+    entities: [],
+    synchronize: true,
+  }), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
